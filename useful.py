e347b9644919d0947b6c3b189a51cd6e45d90304
mihassin <marko.hassinen@helsinki.fi>
2017-02-03 16:20:23 +0200

    Update

--- useful.py
+++ useful.py
@@ -1,3 +1,4 @@
 import numpy as np
 if __name__ == "__main__":
-  str.join('', np.random.choice(list("abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRTSUVXYZ1234567890"), 15))
+  pwdstr = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRTSUVXYZ1234567890`~!@#$%^&*()_-+={}[]\|:;'<>,.?/" + '"'
+  str.join('', np.random.choice(list(pwdstr), 15))
